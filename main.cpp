f8abc85e9ae84e9a1ec49a13ec42812a791353c1
gabrirusso <91951339+gabrirusso@users.noreply.github.com>
2021-11-13 10:50:09 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
-using namespace std;
 
+using namespace std;
 int main() {
-   return 0;
+    int a = 0, b = 1, c = a+b;
+    int n=0;
+    cin>>n;
+    while(n<2){
+        cout<<"errore"<<endl;
+        cin>>n;
+    }
+cout<<1<<endl;
+    for(int i=1; i<n; i++) {
+        cout << c << endl;
+        a=b;
+        b=c;
+        c=a+b;
+    }
+    return 0;
 }
