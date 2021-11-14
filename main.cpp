19d5ae1a55684b24c9b1c07a8c68f05b4ff8974d
gabrirusso <91951339+gabrirusso@users.noreply.github.com>
2021-11-14 23:12:31 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,16 +5,17 @@ int main() {
     int a = 0, b = 1, c = a+b;
     int n=0;
     cin>>n;
-    while(n<2){
+    if(n>=2) {
+
+        cout << 1 << endl;
+        for (int i = 1; i < n; i++) {
+            cout << c << endl;
+            a = b;
+            b = c;
+            c = a + b;
+        }
+    }else{
         cout<<"errore"<<endl;
-        cin>>n;
-    }
-cout<<1<<endl;
-    for(int i=1; i<n; i++) {
-        cout << c << endl;
-        a=b;
-        b=c;
-        c=a+b;
     }
     return 0;
 }
